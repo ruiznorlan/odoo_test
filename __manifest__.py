9bbfe31c352bc128bab1277c67c55ec7d1c22d1e
Norlan Ruiz <ruiznorlan@gmail.com>
2022-04-29 18:48:26 -0600

    [INIT] Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Odoo Demo',
+    'version': '15.0.1.0.0',
+    'author': 'Norlan Ruiz',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/ruiznorlan/odoo_test',
+    'category': 'Sales/CRM',
+    'depends': [
+        'base',
+        'contacts'
+        ],
+    'data': [
+        'views/res_partner_view.xml',
+        'data/res_partner_demo.xml'
+    ]
+}
---
+++ models/res_partner.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api, _
+
+
+class PartnerInherit(models.Model):
+    _inherit = 'res.partner'
+    _description = 'Contact'
+
+    last_name = fields.Char(index=True, tracking=True)
+
+    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name', 'last_name')
+    def _compute_display_name(self):
+        diff = dict(show_address=None, show_address_only=None, show_email=None, html_format=None, show_vat=None)
+        names = dict(self.with_context(**diff).name_get())
+        for partner in self:
+            partner.display_name = names.get(partner.id) if not partner.last_name else  names.get(partner.id) + ' ' + partner.last_name
